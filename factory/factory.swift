029087fa4b523fd7b3b47229858bd6f9b7f85922
@bany <53461381+9bany@users.noreply.github.com>
2022-06-02 22:00:02 +0700

    ft/inner-factory (#13)

--- factory/factory.swift
+++ factory/factory.swift
@@ -3,12 +3,12 @@ import Foundation
 class Point: CustomStringConvertible {
     var x, y: Double
 
-    init(x: Double, y: Double) {
+    private init(x: Double, y: Double) {
         self.x = x
         self.y = y
     }
 
-    init(rho: Double, theta: Double) {
+    private init(rho: Double, theta: Double) {
         x = rho * cos(theta)
         y = rho * sin(theta)
     }
@@ -16,21 +16,28 @@ class Point: CustomStringConvertible {
     var description: String {
         return "x= \(self.x), y= \(self.y)"
     }
+
+    static let factory = PointFactory.instance
+    class PointFactory {
+        
+        private init() {}
+        
+        static let instance = PointFactory()
+
+        func createCartesian(x: Double, y: Double) -> Point {
+            return Point(x: x, y: y)
+        }
+
+        func createPolar(rho: Double, theta: Double) -> Point {
+            return Point(rho: rho, theta: theta)
+        }
+    }
 }
 
-class PointFactory {
-    
-    static func createCartesian(x: Double, y: Double) -> Point {
-        return Point(x: x, y: y)
-    }
 
-    static func createPolar(rho: Double, theta: Double) -> Point {
-        return Point(rho: rho, theta: theta)
-    }
-}
 
 func main() {
-    let point = PointFactory.createPolar(rho: 1, theta: 2)
+    let point = Point.factory.createPolar(rho: 1, theta: 2)
     print(point)
 }
 
