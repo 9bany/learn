d32db54ef3b9bf44be4ceec98916c8c212bea30d
@bany <53461381+9bany@users.noreply.github.com>
2022-06-09 21:22:49 +0700

    Ft/abstract factory (#15)
    * ft/abstract_factory
    
    * ft/factory

--- factory/factory.swift
+++ factory/factory.swift
@@ -42,3 +42,24 @@ func main() {
 }
 
 main()
+
+
+class Person
+{
+  var id: Int
+  var name: String
+
+  init(called name: String, withId id: Int)
+  {
+    self.name = name
+    self.id = id
+  }
+}
+
+class PersonFactory
+{
+  func createPerson(name: String) -> Person
+  {
+    return Person(called: name, withId: Date.now.timeIntervalSince1970)
+  }
+}
